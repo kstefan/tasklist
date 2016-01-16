269b5f97b733a6f9071dd9007c7088e9d6779839
Karel Stefan <karel.stefan@etnetera.cz>
2016-01-16 21:35:23 +0100

    Allow rest api

--- src/main/java/cz/karelstefan/tasklist/SecurityConfig.java
+++ src/main/java/cz/karelstefan/tasklist/SecurityConfig.java
@@ -2,6 +2,7 @@ package cz.karelstefan.tasklist;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,5 +13,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
     }
 }
