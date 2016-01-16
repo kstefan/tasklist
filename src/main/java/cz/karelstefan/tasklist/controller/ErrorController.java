a455b4deffa51d815d91d54ef04ed6f57fb8c4ae
Karel Stefan <karel.stefan@etnetera.cz>
2016-01-16 21:34:44 +0100

    Show more detailed errors

--- src/main/java/cz/karelstefan/tasklist/controller/ErrorController.java
+++ src/main/java/cz/karelstefan/tasklist/controller/ErrorController.java
@@ -1,12 +1,28 @@
 package cz.karelstefan.tasklist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Map;
 
 @Controller
 public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController {
 
     private static final String ERROR_PATH = "/error";
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @Autowired
+    Environment env;
 
     @Override
     public String getErrorPath() {
@@ -14,12 +30,19 @@ public class ErrorController implements org.springframework.boot.autoconfigure.w
     }
 
     @RequestMapping(ERROR_PATH)
-    public String error() {
+    public String error(HttpServletRequest request, Model model) {
+        if (Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+            model.addAllAttributes(getErrorAttributes(request));
+        }
         return "error";
     }
 
     @RequestMapping("/404")
     public String error404() {
         return "404";
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request) {
+        return errorAttributes.getErrorAttributes(new ServletRequestAttributes(request), true);
     }
 }
